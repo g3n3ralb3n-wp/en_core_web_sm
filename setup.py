e6db4499f0fc39a75cfc3595ca5caa1041d6cf8e
g3n3ralb3n-wp <51753752+g3n3ralb3n-wp@users.noreply.github.com>
2019-06-13 09:53:14 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Benjamin Johnson',                   # Type in your name
   author_email = 'benjaminallanjohnson@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/g3n3ralb3n-wp/spacy-dictionary-sm',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/g3n3ralb3n-wp/spacy-dictionary-sm/archive/v0.1.tar.gz',    # I explain this later on
   keywords = ['Spacy', 'NLP', 'dictionary'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'spacy',
